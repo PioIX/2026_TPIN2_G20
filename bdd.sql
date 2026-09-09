396bbfc49997a234538cf18be3a518eb20829a8d
martin <mrebora@pioix.edu.ar>
2026-09-09 09:43:18 -0300

    base de datos completa
    Co-Authored-By: fnille <240753625+fnille@users.noreply.github.com>

--- bdd.sql
+++ bdd.sql
@@ -1,14 +1,21 @@
+DROP TABLE IF EXISTS mensajes;
+DROP TABLE IF EXISTS chats;
+DROP TABLE IF EXISTS usuarioWP;
+
+
 CREATE TABLE usuarioWP ( 
     idUsuario INT PRIMARY KEY, 
     nombre VARCHAR(500), 
     mail VARCHAR(500), 
-    contrasenia VARCHAR(500) 
+    contrasenia VARCHAR(500),
+    foto VARCHAR(500)
 ); 
 
 CREATE TABLE chats ( 
     idChat INT PRIMARY KEY,
     nombre VARCHAR(500),    
-    esGrupo BOOLEAN         
+    esGrupo BOOLEAN,      
+    foto VARCHAR(500)
 );
 
 CREATE TABLE mensajes ( 
@@ -20,4 +27,11 @@ CREATE TABLE mensajes (
     FOREIGN KEY (idChat) REFERENCES chats(idChat), 
     FOREIGN KEY (idUsuario) REFERENCES usuarioWP(idUsuario)
 ); 
+
+CREATE TABLE chatXusuario (
+    idChat INT,
+    idUsuario INT,
+    FOREIGN KEY (idChat) REFERENCES chat(id),
+    FOREIGN KEY (idUsuario) REFERENCES usuario(id)
+);
 
